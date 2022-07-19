f51be986771971b99889b0a3384432b0142436cd
Gilberto Olivas <jose.olivas@bigcommerce.com>
2022-07-18 18:14:13 -0700

    Add new aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -20,10 +20,10 @@ alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
 alias gs="git status -sb"
 alias gf="git fetch --all -p"
 alias gps="git push"
-alias gpsup="git push -u origin $(git branch --show-current)"
 alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
 alias gb="git branch"
+alias gpsup="git push -u origin $(gb --show-current)"
 alias gl='$DOTLY_PATH/bin/dot git pretty-log'
 
 # Utils
